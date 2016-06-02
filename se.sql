98a440ba603987b782e146f390ecf3c7e929a6c1
sunyongjie <sunyongjie1984@gmail.com>
2016-06-02 10:22:15 +0800

    init

---
+++ se.sql
@@ -0,0 +1,24 @@
+set pagesize 30;
+
+set lin 200;
+
+set pause on;
+
+col 姓名 format a10;
+col 姓名 format a10;
+
+col 部门 format a25;
+
+col 最后打卡时间 format a15;
+
+col 星期 format a10;
+
+col 餐补金额 format 99999999;
+
+col 员工号 format a10;
+
+col 当日小时数 format 99999999;
+
+col 姓名 format a10;
+
+col 日期 format a15;

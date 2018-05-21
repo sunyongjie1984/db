1836019f576f79b859f6cc514fe32a36ed3a544a
yongjies <yongjies@cadence.com>
2018-05-21 17:56:02 +0800

    modify

--- sqlOptimize/1_1_2.sql
+++ sqlOptimize/1_1_2.sql
@@ -1,2 +1,8 @@
 select * from emp
     where mgr = 7902
+select count( * ) from emp;
+select count( distinct mgr ) from emp;
+// the following statement can not run in file
+// I have to copy and paste this line in sqlplus window to run them
+exec dbms_stats.gather_table_stats(ownname => 'SCOTT', tabname => 'EMP', estimate_percent => 100,
+    cascade => true, method_opt =>'for all columns size 1', no_invalidate ->false);

bab835b379ea7686712f54fcd06c0bf80fb3279c
yongjies <yongjies@cadence.com>
2018-05-28 17:20:29 +0800

    modify

--- backup.bat
+++ backup.bat
@@ -1,4 +1,17 @@
 #exp hr/hr@thinkpad file=d:\git\db\hr.dmp
 #exp scott/tiger_1234@thinkpad file=d:\git\db\scott.dmp
+set LANG=en-US
 
-exp scott/tiger@orcl file=d:\git\db\orcl_scott.dmp
+
+
+for /f %%I in ('wmic os get localdatetime ^|find "20"') do set dt=%%I
+REM dt format is now YYYYMMDDhhmmss...
+set dt=%dt:~4,2%-%dt:~2,2%-%dt:~0,4%
+echo %dt%
+
+set filename=local_orcl_scott_%dt%.dmp
+echo %filename%
+exp scott/tiger@orcl file=%filename%
+
+
+

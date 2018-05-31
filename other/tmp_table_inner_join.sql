20f127448c027c919de329170b7da718b5de669b
yongjies <yongjies@cadence.com>
2018-05-31 08:47:38 +0800

    modify

--- other/tmp_table_inner_join.sql
+++ other/tmp_table_inner_join.sql
@@ -1,7 +1,18 @@
 select t1.empno, t1.ename, t1.sal, t1.deptno
-    from emp t1
-    inner join (
-        select t2.deptno, max( t2.sal ) max_sal
-        from emp t2
-        group by t2.deptno ) t3
-    on ( t1.deptno = t3.deptno and t1.sal = t3.max_sal )
+from emp t1
+inner join (
+    select t2.deptno, max( t2.sal ) max_sal
+    from emp t2
+    group by t2.deptno ) t3
+on ( t1.deptno = t3.deptno and t1.sal = t3.max_sal )
+
+
+select t1.empno, t1.ename, t1.deptno, t1.sal
+from emp t1
+inner join (
+    select t2.deptno, max(sal) max_sal
+    from emp t2
+    group by t2.deptno )
+t3
+on ( t1.deptno = t3.deptno and t1.sal = t3.max_sal )
+order by t1.sal

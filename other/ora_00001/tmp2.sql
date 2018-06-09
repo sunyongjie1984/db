e388c60b98e57059e2845e709429053e92856da9
sunyongjie <sunyongjie1984@gmail.com>
2018-06-10 07:10:49 +0800

    modify

---
+++ other/ora_00001/tmp2.sql
@@ -0,0 +1,13 @@
+prompt Importing table D2...
+set feedback off
+set define off
+insert into ora00001emp2 (ename, job)
+values ('867cbaef-5cdd-4410-991b-6ed983bd3062', 't1');
+
+insert into ora00001emp2 (ename, job)
+values ('afb3a66c-50f7-4ec5-b13c-712fd701c871', 't2');
+
+insert into ora00001emp2 (ename, job)
+values ('8a625449-64b8-457d-b625-663be72b49ac', 't3');
+
+prompt Done.
